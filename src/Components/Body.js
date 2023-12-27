2b6f81cdbfdff8c00e55a2e7de0dd906cdb1aa6d
Kaveri Chougule <chougulekaveri123@gmail.com>
2023-12-28 02:43:19 +0530

    minor changes updated

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -4,15 +4,21 @@ import DisplayInfo from "./DisplayInfo";
 import React from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
 export default function Book() {
 let [userInput,setUserInupt]=useState("");
 let [arr,setArray]=useState([]);
+useEffect(()=>{
+    setArray(JSON.parse(localStorage.getItem("key")))
+},[])
   function InputUser(ele){
     setUserInupt(ele.target.value);
   }
   function AddItem(){
     if(userInput!=""){
+        let array=[...arr,{userInput}] 
         setArray([...arr,{userInput}])
+        localStorage.setItem("key",JSON.stringify(array));
     }
     setUserInupt("");
     toast.success("Item added to the list !", {
@@ -20,10 +26,12 @@ let [arr,setArray]=useState([]);
     });
   }
   function deleteItem(IdToDelete){
+
     let filteredList=arr.filter((ele,index)=>{
         return index!=IdToDelete; 
     });
     setArray(filteredList)
+    localStorage.setItem("key",JSON.stringify(filteredList));
   }
   
   return (
